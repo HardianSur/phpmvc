cb2ef25f9325e1264dc9c3fdff83d587e088af4f
HardianSur <adihardian06s@gmail.com>
2023-12-29 15:08:01 +0700

    new file:   app/config/config.php 	modified:   app/controllers/Mahasiswa.php 	deleted:    app/core/Constans.php 	new file:   app/core/Database.php 	modified:   app/init.php 	modified:   app/models/Mahasiswa_model.php 	new file:   app/views/mahasiswa/detail.php 	modified:   app/views/mahasiswa/index.php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+const BASEURL = 'http://localhost/BELAJAR_MVC/public';
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_Name', 'phpmvc');
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,17 +1,61 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col-6">
+
+            <button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+
             <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $mhs['nama'] ?>
+                        <a href="<?= BASEURL ?> /mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary ">detail</a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </div>
+</div>
 
-            <?php foreach ($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li>Nama : <?= $mhs['nama'] ?></li>
-                    <li>nrp : <?= $mhs['nrp'] ?></li>
-                    <li>email : <?= $mhs['email'] ?></li>
-                    <li>jurusan : <?= $mhs['jurusan'] ?></li>
-                </ul>
-            <?php endforeach ?>
 
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="formModalLabel">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="NRP" class="form-label">NRP</label>
+                        <input type="number" class="form-control" id="NRP" name="nrp">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="Email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="Email" name="email">
+                    </div>
+
+                    <select class="form-select" aria-label="Default select example" name="jurusan">
+                        <option selected disabled>Open this select menu</option>
+                        <option value="Informatika">Informatika</option>
+                        <option value="Teknik Mesin">Teknik Mesin</option>
+                        <option value="Kehutanan">Kehutanan</option>
+                    </select>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
